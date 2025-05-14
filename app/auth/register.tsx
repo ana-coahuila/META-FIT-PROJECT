54debf8ab78a6059e9758ae47b8357e0d0737be4
NATHAN HERNANDEZ MORENO <133732798+NATHANHERNANDEZ14@users.noreply.github.com>
2025-05-14 00:48:14 -0600

    Update register.tsx
    1

---
+++ app/auth/register.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function register() {
+  return (
+    <View>
+      <Text>Bienvenido al registro</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
